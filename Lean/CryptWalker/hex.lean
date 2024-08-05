ddf894382a8a8e6f1044cb8766e4afd15ef4ebfc
David Stainton <dstainton415@gmail.com>
2024-08-05 10:52:35 -0700

    Add hex

---
+++ Lean/CryptWalker/hex.lean
@@ -0,0 +1,11 @@
+
+def byteToHex (b : UInt8) : String :=
+  let high := hexDigitRepr (b.toNat / 16)
+  let low := hexDigitRepr (b.toNat % 16)
+  s!"{high}{low}"
+
+def byteArrayToHex (blob : ByteArray) : String :=
+  blob.foldl (fun acc b => acc ++ byteToHex b) ""
+
+instance : Repr ByteArray where
+  reprPrec a _ := byteArrayToHex a
