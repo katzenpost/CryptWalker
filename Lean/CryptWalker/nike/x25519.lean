6d0d71b8a947e0033111db016b61e83b66008c33
David Stainton <dstainton415@gmail.com>
2024-08-20 19:33:21 -0700

    rename x25519 files

--- Lean/CryptWalker/nike/x25519.lean
+++ Lean/CryptWalker/nike/x25519.lean
@@ -1,53 +1,118 @@
 /-
 SPDX-FileCopyrightText: © 2024 David Stainton
 SPDX-License-Identifier: AGPL-3.0-only
-
-Inspired by the Haskell FFI X25519:
-https://github.com/haskell-crypto/cryptonite/blob/master/Crypto/PubKey/Curve25519.hs
 -/
-
+import Mathlib.Algebra.Field.Defs
+import Mathlib.Algebra.Field.Basic
+import Mathlib.Data.ZMod.Basic
 import Mathlib.Data.ByteArray
+
+import CryptWalker.util.newnat
+import CryptWalker.util.newhex
 import CryptWalker.nike.nike
 
-namespace CryptWalker.nike.x25519
+namespace CryptWalker.nike.x25519pure
 
-def keySize : Nat := 32
+def p : ℕ := 2^255 - 19
+def basepoint : ZMod p := 9
+def keySize : ℕ := 32
 
-@[extern "curve25519"]
-opaque curve25519 : ByteArray → ByteArray → ByteArray
+theorem p_is_prime : Nat.Prime p := by sorry
+instance fact_p_is_prime : Fact (Nat.Prime p) := ⟨p_is_prime⟩
+instance : Field (ZMod p) := ZMod.instField p
 
-def dh (privateKey : ByteArray) (publicKey : ByteArray) : ByteArray :=
-  if privateKey.size ≠ keySize then
-    panic! "Private key must be 32 bytes long."
-  else if publicKey.size ≠ keySize then
-    panic! "Public key must be 32 bytes long."
-  else
-    curve25519 privateKey publicKey
+def clampScalarBytes (scalarBytes : ByteArray) : ByteArray :=
+  let clamped1 := scalarBytes.set! 0 (scalarBytes.get! 0 &&& 0xf8)
+  let clamped2 := clamped1.set! 31 ((clamped1.get! 31 &&& 0x7f) ||| 0x40)
+  clamped2
 
-def toPublic (privateKey : ByteArray) : ByteArray :=
-  dh privateKey basepoint
-  where
-  basepoint : ByteArray := String.toAsciiByteArray "\x09\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+def fromField (x : ZMod p) : ByteArray :=
+  let bytes := ByteArray.mk $ Array.mk $ (ByteArray.toList $ natToBytes x.val).reverse
+  bytes ++ ByteArray.mk (Array.mk (List.replicate (keySize - bytes.size) 0))
 
-def x25519generatePrivateKey : IO ByteArray := do
-  let mut arr := ByteArray.mkEmpty 32
-  for _ in [0:32] do
-    let randomByte ← IO.rand 0 255
-    arr := arr.push (UInt8.ofNat randomByte)
-  return arr
+def toField (ba : ByteArray) : ZMod p :=
+  let n := (ByteArray.mk $ Array.mk ba.toList.reverse).foldl (fun acc b => acc * 256 + b.toNat) 0
+  n
+
+def clampScalar (scalar : ZMod p) : ZMod p :=
+  let b := fromField scalar
+  let newB := clampScalarBytes b
+  toField newB
+
+structure LadderState :=
+  (x1 : ZMod p)
+  (x2 : ZMod p)
+  (z2 : ZMod p)
+  (x3 : ZMod p)
+  (z3 : ZMod p)
+
+def montgomery_step (s : LadderState) : LadderState :=
+  let tmp0 := s.x3 - s.z3
+  let tmp1 := s.x2 - s.z2
+  let x2 := s.x2 + s.z2
+  let z2 := s.x3 + s.z3
+  let z3 := (tmp0 * x2)
+  let z2 := z2 * tmp1
+  let tmp0 := tmp1^2
+  let tmp1 := x2^2
+  let x3 := z3 + z2
+  let z2 := z3 - z2
+  let x2 := tmp1 * tmp0
+  let tmp1 := tmp1 - tmp0
+  let z2 := z2^2
+  let z3 := tmp1 * 121666
+  let x3 := x3^2
+  let tmp0 := tmp0 + z3
+  let z3 := s.x1 * z2
+  let z2 := tmp1 * tmp0
+  { s with x2 := x2, z2 := z2, x3 := x3, z3 := z3 }
+
+def montgomery_ladder (scalar : ZMod p) (point : ZMod p) : LadderState :=
+  let e : ByteArray := fromField scalar
+  let initState : LadderState := {
+    x1 := point,
+    x2 := 1,
+    z2 := 0,
+    x3 := point,
+    z3 := 1
+  }
+  let finalState := (List.range 255).reverse.foldl (fun (state, swap) pos =>
+    let byteIndex := pos / 8
+    let bitIndex := pos % 8
+    let b : UInt8 := Nat.toUInt8 ((e.get! byteIndex).toNat >>> bitIndex) &&& 1
+    let newSwap := swap ^^^ b
+    let (x2, x3) := if newSwap == 1 then (state.x3, state.x2) else (state.x2, state.x3)
+    let (z2, z3) := if newSwap == 1 then (state.z3, state.z2) else (state.z2, state.z3)
+    let newState := montgomery_step { state with x2 := x2, x3 := x3, z2 := z2, z3 := z3 }
+    (newState, b)
+  ) (initState, 0)
+
+  let finalSwap := finalState.snd
+  let finalState := finalState.fst
+  let (x2, x3) := if finalSwap == 1 then (finalState.x3, finalState.x2) else (finalState.x2, finalState.x3)
+  let (z2, z3) := if finalSwap == 1 then (finalState.z3, finalState.z2) else (finalState.z2, finalState.z3)
+  { finalState with x2 := x2, x3 := x3, z2 := z2, z3 := z3 }
+
+def scalarmult (scalarBytes : ByteArray) (point : ZMod p) : ZMod p :=
+  let clampedScalar := toField $ clampScalarBytes scalarBytes
+  let finalState := montgomery_ladder clampedScalar point
+  finalState.x2 * finalState.z2⁻¹
+
+def curve25519 (scalarBytes : ByteArray) (point : ByteArray) : ByteArray :=
+  fromField $ scalarmult scalarBytes $ toField point
 
 /-
   NIKE type classes instances for x25519
 -/
+
+def PublicKeySize := keySize
+def PrivateKeySize := keySize
+
 structure PrivateKey where
   data : ByteArray
 
 structure PublicKey where
   data : ByteArray
-
-def defaultPublicKey : PublicKey :=
-  let defaultPrivateKey : ByteArray := ByteArray.mk (Array.mk (List.replicate 32 (0 : UInt8)))
-  PublicKey.mk (toPublic defaultPrivateKey)
 
 instance : nike.Key PrivateKey where
   encode : PrivateKey → ByteArray := fun (key : PrivateKey) => key.data
@@ -59,23 +124,31 @@ instance : nike.Key PublicKey where
 
 structure X25519Scheme
 
+def generatePrivateKey : IO PrivateKey := do
+  let mut arr := ByteArray.mkEmpty keySize
+  for _ in [0:keySize] do
+    let randomByte ← IO.rand 0 255
+    arr := arr.push (UInt8.ofNat randomByte)
+  pure { data := arr }
+
+def derivePublicKey (sk : PrivateKey) : PublicKey :=
+    PublicKey.mk $ fromField $ (scalarmult sk.data basepoint)
+
 instance : nike.NIKE X25519Scheme where
   PublicKeyType := PublicKey
   PrivateKeyType := PrivateKey
 
   generatePrivateKey : IO PrivateKey := do
-    let key : ByteArray ← x25519generatePrivateKey
-    pure { data := key }
+    generatePrivateKey
 
   generateKeyPair : IO (PublicKey × PrivateKey) := do
-    let rawprivKey ← x25519generatePrivateKey
-    let privKey := PrivateKey.mk rawprivKey
-    let pubKey := PublicKey.mk (toPublic privKey.data)
+    let privKey ← generatePrivateKey
+    let pubKey := derivePublicKey privKey
     pure (pubKey, privKey)
 
-  derivePublicKey (sk : PrivateKey) : PublicKey := PublicKey.mk $ toPublic sk.data
+  derivePublicKey (sk : PrivateKey) : PublicKey := derivePublicKey sk
 
-  groupAction (sk : PrivateKey) (pk : PublicKey) : PublicKey := PublicKey.mk $ dh sk.data pk.data
+  groupAction (sk : PrivateKey) (pk : PublicKey) : PublicKey := PublicKey.mk $ curve25519 sk.data pk.data
 
   privateKeySize : Nat := keySize
   publicKeySize : Nat := keySize
@@ -85,4 +158,4 @@ instance : nike.NIKE X25519Scheme where
   encodePublicKey : PublicKey → ByteArray := fun (pk : PublicKey) => nike.Key.encode pk
   decodePublicKey : ByteArray → Option PublicKey := fun (bytes : ByteArray) => nike.Key.decode bytes
 
-end CryptWalker.nike.x25519
+end CryptWalker.nike.x25519pure
