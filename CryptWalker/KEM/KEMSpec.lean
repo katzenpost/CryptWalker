6b6536f831a739a572baa43f5cec8ace7a1733c2
David Stainton <dstainton415@gmail.com>
2026-08-21 15:37:08 +0200

    Fix proofs for KEMSpec

--- CryptWalker/KEM/KEMSpec.lean
+++ CryptWalker/KEM/KEMSpec.lean
@@ -66,11 +66,21 @@ def IsEncapsulation (sk : PrivateKey) (c : Ciphertext) (k : Plaintext) :=
 def KeyPair (pk : PublicKey) (sk : PrivateKey) :=
   ∀ s, let (c, k) := (encap pk s).1; IsEncapsulation sk c k
 
-theorem generate_ok : SatisfiesM (fun (pk, sk) => KeyPair pk sk) generate := by
-  simp [generate]; intro s; split; rename_i h1 s1 _; exact h1
+theorem SatisfiesM_StateM_eq {σ α} {p : α → Prop} {x : StateM σ α} :
+    SatisfiesM p x ↔ ∀ s, p (x s).1 := by
+  constructor
+  · rintro ⟨x', rfl⟩ s
+    exact (x' s).1.2
+  · intro h
+    exact ⟨fun s => (⟨(x s).1, h s⟩, (x s).2), rfl⟩
+
+theorem generate_ok : SatisfiesM (fun (pk, sk) => KeyPair pk sk) generate :=
+  SatisfiesM_StateM_eq.2 fun s => (encDecSpec.generate s).1.2.2
 
 theorem encap_ok {pk sk} (h : KeyPair pk sk) :
-    SatisfiesM (fun (c, k) => IsEncapsulation sk c k) (encap pk) := by simpa using h
+    SatisfiesM (fun (c, k) => IsEncapsulation sk c k) (encap pk) :=
+  SatisfiesM_StateM_eq.2 h
 
-theorem decap_ok {sk c k} (h : IsEncapsulation sk c k) :
-    SatisfiesM (fun k' => k' = k) (decap sk c) := by simpa using h
+theorem decap_ok {sk ct k} (h : IsEncapsulation sk ct k) :
+    SatisfiesM (fun k' => k' = k) (decap sk ct) :=
+  SatisfiesM_StateM_eq.mpr h
